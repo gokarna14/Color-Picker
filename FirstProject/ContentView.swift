d08dde04880a3fcf1ee1fb9b376640d334cda1e3
Gokarna Adhikari <gokarna.adhikari14@gmail.com>
2023-04-11 19:35:09 +0545

    Initial Commit

---
+++ FirstProject/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  FirstProject
+//
+//  Created by Prabas Adhikari on 11/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isTextShowing = true
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            if(isTextShowing)
+            {
+                Text("Hello, world!")
+            }
+            else
+            {
+                Text("The Text is chenged !")
+            }
+            
+            Button("Click to see magic!") {
+                isTextShowing = !isTextShowing
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Prabas Adhikari on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
